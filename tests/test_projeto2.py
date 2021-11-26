3ea1048d0815364169e80a4bf5c5761bbe0c9cd5
Enderson Menezes (Mr. Enderson) <endersonster@gmail.com>
2021-11-26 19:59:56 -0300

    Update test_projeto2.py

--- tests/test_projeto2.py
+++ tests/test_projeto2.py
@@ -12,4 +12,13 @@ def test_boolean():
 def test_conta_desafio_1():
   assert 90 > main.desafio1 > 100
   
+def test_syntax_desafio():
+  # Abrir o arquivo submission.projeto2
+  # with open(.....) as file
+  # Achar a linha desafio1 = **^$#^%#
+  # file.read(), achar a linha `desafio1 =`
+  # split = -> validar o indice 1 do array
+  # Verifica se nessa linha tem o valor 55, e possui uma operação valida. (+, -, , *, ^)
+  assert True
+  
  
