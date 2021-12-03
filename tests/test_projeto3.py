6232a814f86f7163e3f9734fd1939ef593e58fe6
Enderson Menezes (Mr. Enderson) <endersonster@gmail.com>
2021-12-03 20:08:51 -0300

    Update test_projeto3.py

--- tests/test_projeto3.py
+++ tests/test_projeto3.py
@@ -1,4 +1,15 @@
+import os
 from submission.projeto3 import main
 
 def test_se_copiou_tarefa1():
   assert main.lista_de_compras == ['Banana', 'Arroz', 'Laranja']
+  
+def test_se_if_existe_tarefa1():
+  with open('submission/projeto3/main.py', 'r') as file:
+    tmp_file = file.read()
+  verifica = False
+  if "'Uva' in lista_de_compras" in tmp_file:
+    verifica = True
+  elif '"Uva" in lista_de_compras' in tmp_file:
+    verifica = True    
+  assert verifica
