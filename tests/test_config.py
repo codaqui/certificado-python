a47010940547b5610ef142215b945299fa4c0ffb
Enderson Menezes (Mr. Enderson) <endersonster@gmail.com>
2022-02-11 19:48:14 -0300

    Update test_config.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,7 @@
 # TODO Teste para Verificar arquivos básicos de resposta
 
-def verifica_se_arquivo_projeto1_existe():
+def test_verifica_se_arquivo_projeto1_existe():
   assert True
 
-def verifica_se_arquivo_projeto2_existe():
+def test_verifica_se_arquivo_projeto2_existe():
   assert True
