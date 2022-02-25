d1bf35dda7fb4600a8eb62d393d257c5def411a4
Enderson Menezes (Mr. Enderson) <endersonster@gmail.com>
2022-02-25 19:37:04 -0300

    Update test_projeto2.py

--- tests/test_projeto2.py
+++ tests/test_projeto2.py
@@ -13,13 +13,15 @@ def test_conta_desafio_1():
   assert 90 < main.desafio1 < 100
   
 def test_syntax_desafio():
-  # Abrir o arquivo submission.projeto2
-  # with open(.....) as file
-  # Achar a linha desafio1 = **^$#^%#
-  # file.read(), achar a linha `desafio1 =`
-  # split = -> validar o indice 1 do array
-  # Verifica se nessa linha tem o valor 55, e possui uma operação valida. (+, -, , *, ^)
-  assert True
+  valido = False
+  with open("submission/projeto2/main.py", "r") as file:
+    temp = file.readlines()
+    for line in temp:
+      if 'desafio1' in line:
+        line_array = list(line)
+        if '+' in line_array or '-' in line_array or '*' in line_array or '/' in line_array:
+          valido = True
+  assert valido
   
 def test_dinheiro():
   assert main.dinheiro == 7.5
